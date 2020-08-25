b9267087ef618ea8ec044354726744c4c571a681
Koivula1 <oskari.koivula@ict.opetus.local>
2020-08-25 08:48:56 +0300

    20200825

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/layout/Header';
 import Search from './components/layout/Search';
+import Jobs from './components/Jobs';
 
 function App() {
 
   const initJobs = []
   const [jobs, setJobs] = useState(initJobs);
 
-  fetch('http://gis.vantaa.fi/rest/tyopaikat/v1/kaikki')
-    .then(response => response.json())
-    .then(json => setJobs([...json]));
+  useEffect(() => {
+    fetch('http://gis.vantaa.fi/rest/tyopaikat/v1/kaikki')
+      .then(response => response.json())
+      .then(json => setJobs([...json]));
+  }, []);
 
   const rows = () => jobs.map(job => {
     return <p><input type="checkbox" />{job.tyotehtava}, {job.osoite},<a href={job.linkki}> Lisätietoa</a></p>
@@ -22,6 +25,7 @@ function App() {
       </div>
       <div className="App">
         <Search />
+        <Jobs />
         {rows()}
       </div>
     </div>
