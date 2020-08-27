7ce259006e6a03d44067f993af50bff80abec0e7
Koivula1 <oskari.koivula@ict.opetus.local>
2020-08-27 10:51:35 +0300

    20200827

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
+
 function Weather() {
-
-/*  const initWeather = []
-    const [weather, setWeather] = useState(initWeather);
-
-    fetch('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=439d4b804bc8187953eb36d2a8c26a02')
-    .then(response => response.json())
-    .then(json => setJobs([...json]));
-    */
     return (
         <p>Sääasematietoja</p>
     )
 }
 
-export default Weather;
+export default Weather
