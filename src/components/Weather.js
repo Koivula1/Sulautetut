7c572f743fe9c32cfbfe04f268fa43815a80da41
Koivula1 <oskari.koivula@ict.opetus.local>
2020-09-15 11:56:40 +0300

    20200915

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -14,7 +14,9 @@ function Weather() {
         .then(json => setWeather([...json]));
 
     const rows = () => weather.slice(0, 24).reverse().map(temphum => {
-        return <div>{temphum.PublishedAt} ------<b>Ilmankosteus</b> {temphum.Hum.split('.')[0]}% <b>Lämpötila</b> {temphum.Temp.split('.')[0]}</div>
+        const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + "." + temphum.PublishedAt.split('T')[0].split('-')[1] + "." + temphum.PublishedAt.split('T')[0].split('-')[0]
+        const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[0] + ":" + temphum.PublishedAt.split('T')[1].split(':')[1]
+        return <div><b>Pvm. </b>{measurementDate}, <b>klo: </b>{measurementTime} <b>Ilmankosteus</b> {temphum.Hum.split('.')[0]}% <b>Lämpötila</b> {temphum.Temp.split('.')[0]}°C</div>
     })
 
     return (
@@ -30,50 +32,7 @@ function Weather() {
             </div>
             <h2>Lämpötila</h2>
 
-            <VictoryChart domainPadding={{ x: 30, y: 5 }} width={1000} height={250}
-                containerComponent={<VictoryVoronoiContainer />}
-            >
-                /*<VictoryGroup
-                    color="#c43a31"
-                    labels={({ datum }) => `${datum.y} C`}
-                    labelComponent={
-                        <VictoryTooltip
-                            style={{ fontSize: 10 }}
-                        />
-                    }
-                    data={showTemperature}
-                    ></VictoryGroup>
-                        <VictoryScatter
-                            size={({ active })} => active ? 8 :3}
-                            />
-                <VictoryLine
-                    data={[
-                    { x: "1.1.", actual: 0 },
-                    { x: "2.1.", actual: 5 },
-                    { x: "3.1.", actual: 0 },
-                    { x: "4.1.", actual: 1 },
-                    { x: "5.1.", actual: 2 },
-                    { x: "6.1.", actual: 3 },
-                    { x: "7.1.", actual: 1 },
-                    { x: "8.1.", actual: 2 },
-                    { x: "9.1.", actual: 2 },
-                    { x: "10.1.", actual: 1 },
-                    { x: "11.1.", actual: 2 },
-                    { x: "12.1.", actual: 3 },
-                    { x: "13.1.", actual: 1 },
-                    { x: "14.1.", actual: 2 },
-                    { x: "15.1.", actual: 1 },
-                    { x: "16.1.", actual: 2 }
-                ]}*/
-                    style={{
-                    data:
-                        { stroke: "red", strokeWidth: 4 }
-                }}
-                    x="expreriment"
-                    y="actual"
-                />
-
-            </VictoryChart>*/
+            
         </div>
 
 
