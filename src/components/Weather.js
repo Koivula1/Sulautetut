1b2267581f8a7f4f445c939ddb6f1d39f8a9923c
Koivula1 <oskari.koivula@ict.opetus.local>
2020-09-17 11:32:37 +0300

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -12,14 +12,19 @@ function Weather() {
         .then(response => response.json())
         .then(json => setWeather([...json]));
 
+    let humtempkey = 1;
+    let chartTempData = [];
+    let chartHumData = [];
     const rows = () => weather.slice(0, 24).reverse().map(temphum => {
-        const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + "." + temphum.PublishedAt.
-            split('T')[0].split('-')[1] + "." + temphum.PublishedAt.split('T')[0].split('-')[0]
-        const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[0] + ":" + temphum.PublishedAt.
-            split('T')[1].split(':')[1]
-        return <div><b>Pvm. </b>{measurementDate}, <b>klo: </b>{measurementTime} <b>Ilmankosteus </b>
-            {temphum.Hum.split('.')[0]}% <b>Lämpötila </b>{temphum.Temp.split('.')[0]}°C</div>
+        const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + "." + temphum.PublishedAt.split('T')[0].split('-')[1] + "." + temphum.PublishedAt.split('T')[0].split('-')[0]
+        const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[0] + ":" + temphum.PublishedAt.split('T')[1].split(':')[1]
+        chartTempData.push({ x: String(measurementTime), y: parseInt(temphum.Temp) });
+        chartHumData.push({ experiment: String(measurementTime), actual: parseInt(temphum.Hum), label: String(temphum.Hum.split(".")[0]) + "%" });
+        return <div key={humtempkey++}><b>Pvm. </b>{measurementDate}, <b>klo: </b>{measurementTime} <b>Ilmankosteus </b>{temphum.Hum.split('.')[0]}% <b>Lämpötila </b>{temphum.Temp.split('.')[0]}°C</div>
     })
+    console.log(chartTempData);
+    const showTemperature = chartTempData;
+    const showHumidity = chartHumData;
 
     return (
         <div align="center">
@@ -34,7 +39,53 @@ function Weather() {
             </div>
             <h2>Lämpötila</h2>
 
-
+            <VictoryChart domainPadding={{ x: 30, y: 5 }} width={1000} height={250}
+                containerComponent={<VictoryVoronoiContainer />}
+            >
+                <VictoryGroup
+                    color="#db1407"
+                    labels={({ datum }) => `${datum.y} °C`}
+                    labelComponent={
+                        <VictoryTooltip
+                            style={{ fontSize: 10 }}
+                        />
+                    }
+                    data={showTemperature}
+                >
+                    <VictoryLine 
+                    style={{
+                        data:
+                            { strokeWidth: 3 }
+                    }}
+                    />
+                    <VictoryScatter
+                        color= "black"
+                        size={({ active }) => active ? 8 : 3}
+                    />
+                </VictoryGroup>
+            </VictoryChart>
+            <hr />
+            <h2>Ilmankosteus</h2>
+            <VictoryChart domainPadding={{ x: 30, y: 5 }}
+                width={1000}
+                height={250}>
+                <VictoryBar
+                    style={{ data: { fill: "#6495ED" }, labels: { fontSize: 10 } }}
+                    data={showHumidity}
+                    x="experiment"
+                    y="actual"
+                />
+                <VictoryAxis
+                    style={{
+                        axisLabel: { padding: 30 }
+                    }}
+                />
+                <VictoryAxis dependentAxis
+                    style={{
+                        axisLabel: { padding: 40 }
+                    }}
+                />
+            </VictoryChart>
         </div>
     )
 }
